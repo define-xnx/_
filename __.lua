8045c4e9bbbb0411a60a788f45f6a040a8f3e2b9
define-xnx <100484518+define-xnx@users.noreply.github.com>
2022-02-28 12:46:26 +0300

    Create __.lua

---
+++ __.lua
@@ -0,0 +1,18 @@
+	local Cam = workspace.CurrentCamera
+	local Pos, Char = Cam.CFrame, game.Players.LocalPlayer.Character
+	local Human = Char and Char.FindFirstChildWhichIsA(Char, "Humanoid")
+	local nHuman = Human.Clone(Human)
+	nHuman.Parent, game.Players.LocalPlayer.Character = Char, nil
+	nHuman.SetStateEnabled(nHuman, 15, false)
+	nHuman.SetStateEnabled(nHuman, 1, false)
+	nHuman.SetStateEnabled(nHuman, 0, false)
+	nHuman.BreakJointsOnDeath, Human = true, Human.Destroy(Human)
+	game.Players.LocalPlayer.Character, Cam.CameraSubject, Cam.CFrame = Char, nHuman, wait() and Pos
+	nHuman.DisplayDistanceType = Enum.HumanoidDisplayDistanceType.None
+	local Script = Char.FindFirstChild(Char, "Animate")
+	if Script then
+		Script.Disabled = true
+		wait()
+		Script.Disabled = false
+	end
+	nHuman.Health = nHuman.MaxHealth
